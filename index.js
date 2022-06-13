fa0c9ab62570995052776517187c4775c04fccd2
Eli <+>
2022-06-13 16:03:51 -0600

    commit tree fc8ebf7dc279d90397d1cc543551fc6446ac5697

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+//const recipients = ["Charlie", "Samip", "Ali"];
+const newArray = [];
+//const holiday = "birthday";
+
+function writeCards(recipients, holiday) {
+    for (var i = 0; i < recipients.length; i++) {
+        newArray.push(`Thank you, ${recipients[i]}, for the wonderful ${holiday} gift!`);
+    }
+    
+    return newArray;
+}
+
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
+
